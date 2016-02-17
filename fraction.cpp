8438a6be09a5f09dc0b7be9d458f45af349a4636
jizhouli <JustinLi.ljt@gmail.com>
2016-02-17 17:35:25 +0800

    update fraction.cpp: 1.增加NULL的处理 2.重载<运算符（可作为map的key） 3.NULL作为值域最小值处理

--- fraction.cpp
+++ fraction.cpp
@@ -7,22 +7,42 @@ class Fraction {
 public:
     int _num; //numerator
     int _den; //denominator
-
-    Fraction() : _num(0), _den(1) {}
+    
+    Fraction() : _num(0), _den(0) {}
     Fraction(int x, int y) {
-        int z = gcd(x, y);
-        if (z != 1) {
-            x /= z;
-            y /= z;
+        if (x==0 && y==0) {
+            _num = 0;
+            _den = 0;
         }
-
-        _num = x;
-        _den = y;
+        else if (x!=0 && y==0) {
+            _num = 1;
+            _den = 0;
+        }
+        else {
+            int z = gcd(x, y);
+            if (z != 1) {
+                x /= z;
+                y /= z;
+            }
+            _num = x;
+            _den = y;
+            
+            int sign = 1;
+            if (x*y < 0)
+                sign = -1;
+            _num = abs(x);
+            _den = sign*abs(y);
+        }
     }
-
+    
     int num() const {return _num;}
     int den() const {return _den;}
-
+    bool is_null() const {
+        if (den()==0 && num()==0)
+            return true;
+        return false;
+    }
+    
     /* Greatest Common Divisor */
     int gcd(int a, int b) {
         int c;
@@ -33,9 +53,17 @@ public:
         }
         return b;
     }
+    
+    /* operator overload */
+    bool operator<(const Fraction& a) const;
 };
 
 inline bool operator==(const Fraction& a, const Fraction& b) {
+    /*
+     if (a.is_null() || b.is_null())
+     return false;
+     */
+    
     if (a.num() == b.num() && a.den() == b.den())
         return true;
     else
@@ -46,7 +74,38 @@ inline bool operator!=(const Fraction& a, const Fraction& b) {
     return !(a==b);
 }
 
+bool Fraction::operator<(const Fraction& a) const {
+    /*bug fix because of this code snippet*/
+    if (is_null() && !a.is_null())
+        return true;
+    if (!is_null() && a.is_null())
+        return false;
+    /*end fix*/
+    
+    if (den()==0 && a.den()==0) {
+        /*
+         if (*this==a)
+         return false;
+         */
+        return false;
+    }
+    else if (den()==0 || a.den()==0) {
+        if (den() == 0)
+            return false;
+        else
+            return true;
+    }
+    else {
+        int l = num() * a.den();
+        int r = a.num() * den();
+        return l < r;
+    }
+    return false;
+}
+
 ostream& operator<<(ostream& out, const Fraction& a) {
+    if (a.num()==0 && a.den()==0)
+        return out << "NULL";
     return out << a.num() << "/" << a.den();
 }
 
