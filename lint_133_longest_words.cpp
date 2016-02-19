0ffd73ee8e8686aff38740cedcae13e27bb95ff4
jizhouli <JustinLi.ljt@gmail.com>
2016-02-19 17:52:02 +0800

    lint_133_longest_words.cpp 创建开发环境

---
+++ lint_133_longest_words.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    /**
+     * @param dictionary: a vector of strings
+     * @return: a vector of strings
+     */
+    vector<string> longestWords(vector<string> &dictionary) {
+        vector<string> words = dictionary;
+        return words;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    vector<string> dict;
+    dict.push_back("dog");
+    dict.push_back("google");
+    dict.push_back("facebook");
+    dict.push_back("internationalization");
+    dict.push_back("blabla");
+
+    dict.clear();
+    dict.push_back("like");
+    dict.push_back("love");
+    dict.push_back("hate");
+    dict.push_back("yes");
+    dict.push_back("haha");
+    dict.push_back("go");
+
+    Solution sol;
+    vector<string> words = sol.longestWords(dict);
+    for (vector<string>::iterator it=words.begin(); it!=words.end(); it++)
+        cout << *it << endl;
+
+    return 0;
+}
