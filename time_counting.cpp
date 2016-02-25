40974a313d5c34535ced24cd21f41675f238aec3
jizhouli <JustinLi.ljt@gmail.com>
2016-02-25 16:40:01 +0800

    bug fix time_counting.cpp

--- time_counting.cpp
+++ time_counting.cpp
@@ -2,20 +2,22 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main(int argc, char* argv[]){
   clock_t begin_clock = clock();
-  time_t begin_time = clock();
+  time_t begin_time = time(NULL);
 
   vector<string> str;
-  for(int i=0;i<1000000;i++)
+  for(int i=0;i<1000000*100;i++)
       str.push_back("hello world");
 
   clock_t end_clock = clock();
-  time_t end_time = clock();
-  double elapsed_secs = double(end_clock - begin_clock)/CLOCKS_PER_SEC;
-  cout << "cpu  time(sec) "<< elapsed_secs << endl;
-  cout << "real time(sec) "<< difftime(end_time,begin_time)/1000/1000 << endl;
+  time_t end_time = time(NULL);
+  double cpu_secs = double(end_clock - begin_clock)/CLOCKS_PER_SEC;
+  cout << "cpu  time(sec) "<< cpu_secs << endl;
+  double elapsed_secs = difftime(end_time,begin_time);
+  cout << "real time(sec) "<< fixed << setprecision(5) <<  elapsed_secs << endl;
 }
