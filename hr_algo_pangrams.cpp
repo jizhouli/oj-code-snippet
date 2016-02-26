4c8ea8280b021526df2ab13f520d8870a212c8bd
jizhouli <JustinLi.ljt@gmail.com>
2016-02-26 15:03:05 +0800

    优化为位操作 hr_algo_pangrams.cpp

--- hr_algo_pangrams.cpp
+++ hr_algo_pangrams.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
 #include <string>
+#include <climits> // UINT_MAX
 using namespace std;
 
 int main(int argc, char* argv[]){
     string text;
     //cin >> text;
     getline(cin, text);
+
+    /* BIT MANIPULATION */
+    unsigned int alphabet_signs_bit = 0;
+    for(int i = (sizeof(unsigned int)/sizeof(char)*8); i>=26; i--){
+        alphabet_signs_bit |= 1 << i;
+    }
+
+    for(int i=0;i<text.length();i++){
+        if(('a' <= text[i]) && (text[i] <='z'))
+            alphabet_signs_bit |= 1 << (text[i] - 'a');
+        else if(('A' <= text[i]) && (text[i] <='Z'))
+            alphabet_signs_bit |= 1 << (text[i] - 'A');
+    }
+
+    if (alphabet_signs_bit == UINT_MAX)
+        cout << "pangram" << endl;
+    else
+        cout << "not pangram" << endl;
+
+    /*
     bool is_pangram = true;
     bool alphabet_signs[26] = {false};
-
     for(int i=0;i<text.length();i++){
         if(('a' <= text[i]) && (text[i] <='z'))
             alphabet_signs[text[i] - 'a'] = true;
@@ -26,5 +46,7 @@ int main(int argc, char* argv[]){
         cout << "pangram" << endl;
     else
         cout << "not pangram" << endl;
+    */
+
     return 0;
 }
