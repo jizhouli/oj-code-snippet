ac78f54f44e3db92c2a8e22f9f83334c82f78319
jizhouli <JustinLi.ljt@gmail.com>
2016-03-02 19:25:50 +0800

    优化 lint_76_longest_increasing_subsequence.cpp

--- lint_76_longest_increasing_subsequence.cpp
+++ lint_76_longest_increasing_subsequence.cpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
 class Solution {
@@ -10,6 +12,10 @@ public:
      * @return: The length of LIS (longest increasing subsequence)
      */
     int longestIncreasingSubsequence(vector<int> nums) {
+        return lis_opt(nums);
+    }
+
+    int lis(vector<int> nums){
         int lis_cnt = 0;
         vector<int> dp(nums.size(), 1);
 
@@ -23,10 +29,31 @@ public:
                 lis_cnt = dp[i];
         return lis_cnt;
     }
+
+    int lis_opt(vector<int> nums) {
+        int lis_cnt = 0;
+        if (!nums.size())
+            return lis_cnt;
+
+        vector<int> dp(nums.size(), INT_MAX);
+        dp[0] = nums[0];
+        lis_cnt = 1;
+
+        for(int i=1; i<nums.size(); i++){
+            int j = 0;
+            while(/*j<nums.size() && */dp[j]<=nums[i])
+                j++;
+            dp[j] = nums[i];
+            lis_cnt = max(j+1, lis_cnt);
+            //cout << j << " "<< lis_cnt << endl;
+        }
+        return lis_cnt;
+    }
 };
 
 int main(int argc, char* argv[]){
     int _nums[] = {4,2,4,5,3,7};
+    //int _nums[] = {1,1,1,1};
 
     vector<int> nums(_nums, _nums+sizeof(_nums)/sizeof(_nums[0]));
 
