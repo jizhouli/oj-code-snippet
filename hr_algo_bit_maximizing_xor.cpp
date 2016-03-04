d8b13aa7d0a61e9cec2a6b8b68c22e8b55d9b8dd
jizhouli <JustinLi.ljt@gmail.com>
2016-03-04 15:44:22 +0800

    添加 hr_algo_bit_maximizing_xor.cpp

---
+++ hr_algo_bit_maximizing_xor.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int brute_force_max_xor(int l, int r){
+    int max_xor = 0;
+    for(int i=l;i<=r;i++)
+        for(int j=l;j<=i;j++)
+            if (max_xor < (i^j))
+                max_xor = (i^j);
+    return max_xor;
+}
+
+int max_xor(int l, int r){
+    int i = 1;
+    while (l != r) { l /= 2; r /= 2; i *= 2; }
+    return i - 1;
+}
+
+int maxXor(int l, int r){
+    return max_xor(l, r);
+}
+
+int main(int argc, char* argv[]){
+    int res;
+    int l,r;
+    cin >> l >> r;
+
+    res = maxXor(l, r);
+    cout << res;
+
+    return 0;
+}
